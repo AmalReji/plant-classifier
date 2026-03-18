f64374611c304d44ed0b9374a160b61c67963443
Amal <amalisamazing123@gmail.com>
2026-03-19 08:28:04 +1000

    Confirmed from docs that config.py will work as intended when variables are set in docker

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()  # loads variables from .env ONLY if they are not already set in the environment (e.g. by Docker)
 
 MODEL_DIR = os.getenv("MODEL_DIR", "models")
 MODEL_VERSION = int(os.getenv("MODEL_VERSION"))
