f171998318eec2f1b3fcbb06826b0c614433c9e1
Amal <amalisamazing123@gmail.com>
2026-05-17 19:23:43 +1000

    Set backup MODEL_VERSION in config.py, since .env isn't uploaded to GitHub

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ import os
 load_dotenv()  # loads variables from .env ONLY if they are not already set in the environment (e.g. by Docker)
 
 MODEL_DIR = os.getenv("MODEL_DIR", "/app/models/model_v1")
-MODEL_VERSION = int(os.getenv("MODEL_VERSION"))
+MODEL_VERSION = int(os.getenv("MODEL_VERSION", "1"))
