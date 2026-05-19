80327420040b157dd15830c1869bd4bd95473719
Amal <amalisamazing123@gmail.com>
2026-05-19 21:52:25 +1000

    Central place to assign MODEL_VERSION in model_version.env. Also git ignore cache folders

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # loads variables from .env ONLY if they are not already set in the environment (e.g. by Docker)
+load_dotenv("model_version.env", override=False)  # loads variables from model_version.env ONLY if they are not already set in the environment (e.g. by Docker)
 
-MODEL_DIR = os.getenv("MODEL_DIR", "models/model_v1")
 MODEL_VERSION = int(os.getenv("MODEL_VERSION", "1"))
+MODEL_DIR = os.getenv("MODEL_DIR", f"models/model_v{MODEL_VERSION}")
